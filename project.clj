ea1e3a83bf62a21bd65a7d19c3255ec23d4c7683
Jeremy Hughes <jedahu@gmail.com>
2011-10-17 17:06:44 +1300

    use cluj 0.0.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject
-  me.panzoo/spaghetti "0.0.3-SNAPSHOT"
+  me.panzoo/spaghetti "0.0.3"
 
   :description "Finite State Machine"
 
   :dependencies
-  [[me.panzoo/cluj "0.0.1-SNAPSHOT"]]
+  [[me.panzoo/cluj "0.0.1"]]
 
   :source-path "cljs/src")
