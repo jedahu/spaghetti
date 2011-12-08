90deb085f769da33f8ac4b9fe6521e0421b5c8a7
Jeremy Hughes <jedahu@gmail.com>
2011-12-08 13:16:27 +1300

    refactor for more immutability

--- src/me/panzoo/spaghetti.cljs
+++ src/me/panzoo/spaghetti.cljs
@@ -23,21 +23,22 @@
 (defn state-machine? [x]
   (instance? StateMachine x))
 
-(defn reset
+(defn restart
   "Reset the state machine. If present, `state` becomes the current state,
   otherwise the start state does. If call-callback? is true, the fsm callback
-  is called with a transition argument of `::reset`."
-  [fsm & {:keys [state call-callback?]}]
-  (swap! (:current fsm)
-         (fn [from-state]
-           (let [to-state (or state (:start fsm))]
-             (when call-callback?
-               ((:callback fsm)
-                  {:machine fsm
-                   :from-state from-state
-                   :transition ::reset
-                   :to-state to-state}))
-             to-state))))
+  is called with a transition argument of `::restart`."
+  [fsm & args]
+  (let [{:keys [state call-callback?]} (apply hash-map args)
+        to-state (or state (:start fsm))
+        fsm* (assoc fsm :state to-state)]
+    (if call-callback?
+      (assoc
+        fsm* :data
+        ((:callback fsm)
+           {:from-state (:state fsm)
+            :transition ::restart
+            :to-state to-state}))
+      fsm*)))
 
 (defn state-machine
   "Construct a new state machine. Requires a `start` state and a state `graph`.
@@ -63,36 +64,33 @@
   or `nil`, nothing will be done. It takes the same arguments as the callback.
 
   The start state is automatically entered with a transition of
-  `::reset` from an old state of `start`"
+  `::restart` from an old state of `start`"
   [start graph & {:as opts}]
   (let [fsm (StateMachine.
               start
               (or (:callback opts) (constantly nil))
-              (atom graph)
+              graph
               nil
               {:gensym (gensym)
-               :watchlist (atom #{})
-               :current (atom nil)
+               :watchlist #{}
+               :state nil
                :error (:error opts)
-               :data (atom nil)})]
-    (reset fsm :state start :call-callback? true)
-    fsm))
+               :data nil})]
+    (restart fsm :state start :call-callback? false)))
 
 (defn state
   "Get the current state."
   [fsm]
-  @(:current fsm))
-
-(def foo (atom 1))
+  (:state @fsm))
 
 ;<?
 (check "state-machine"
   (let [graph {:on {:down :off}}
         fsm (state-machine :on graph)]
     (it "should not modify the graph"
-      (expect toEqual graph @(:graph fsm)))
+      (expect toEqual graph (:graph fsm)))
     (it "should start in the start state"
-      (expect toEqual :on (state fsm)))))
+      (expect toEqual :on (:state fsm)))))
 ;?>
 
 (defn act
@@ -104,69 +102,84 @@
   `args` is a map to merge with the callback argument.
   function."
   [fsm trans & args]
-  (swap! (:current fsm)
-         (fn [from-state]
-           (if-let [to-state (get-in @(:graph fsm) [from-state trans])]
-             (do
-               (swap! (:data fsm)
-                      #((:callback fsm)
-                          (merge {:machine fsm
-                                  :from-state from-state
-                                  :transition trans
-                                  :to-state to-state
-                                  :data %}
-                                 (apply hash-map args))))
-               to-state)
-             (do
-               (when-let [err (:error fsm)]
-                 (err
-                   (merge {:machine fsm
-                           :from-state from-state
-                           :transition trans
-                           :to-state nil
-                           :data @(:data fsm)})))
-               from-state)))))
+  (let [from-state (:state fsm)
+        fsm* (assoc
+               fsm :state
+               (or (get-in (:graph fsm) [from-state trans])
+                   from-state))]
+    (if (= from-state (:state fsm*))
+      (do
+        (when-let [err (:error fsm)]
+          (err
+            (merge {:from-state from-state
+                    :transition trans
+                    :to-state nil
+                    :data (:data fsm)}
+                   (apply hash-map args))))
+        fsm*)
+      (assoc
+        fsm* :data
+        ((:callback fsm)
+           (merge {:from-state from-state
+                   :transition trans
+                   :to-state (:state fsm*)
+                   :data (:data fsm)}
+                  (apply hash-map args)))))))
 
 ;<?
 (check "act"
-  (let [fsm (atom nil)
-        err (atom nil)]
-    (before
-      (reset! fsm (state-machine
-                    :on
-                    {:on {:down :off}
-                     :off {:up :on}}
-                    :callback #(conj (or (:data %) [])
-                                     [(:from-state %)
-                                      (:transition %)
-                                      (:to-state %)])
-                    :error #(reset! err [(:from-state %)
-                                         (:transition %)
-                                         (:to-state %)]))))
+  (let [err (atom nil)
+        fsm (state-machine
+              :on
+              {:on {:down :off}
+               :off {:up :on}}
+              :callback #(conj (or (:data %) [])
+                               [(:from-state %)
+                                (:transition %)
+                                (:to-state %)])
+              :error #(reset! err [(:from-state %)
+                                   (:transition %)
+                                   (:to-state %)]))]
     (it "should transition"
-      (act @fsm :down)
-      (expect toEqual :off (state @fsm)))
+      (expect toEqual :off (:state (act fsm :down))))
     (it "should give :callback the correct args"
-      (act @fsm :down)
-      (act @fsm :up)
-      (expect toEqual [[:on :down :off] [:off :up :on]] @(:data @fsm)))
+      (expect toEqual
+              [[:on :down :off] [:off :up :on]]
+              (-> fsm (act :down) (act :up) :data)))
     (it "should call :error on unknown transitions"
-      (act @fsm :unknown)
-      (expect toEqual :on (state @fsm))
+      (expect toEqual :on (:state (act fsm :unknown)))
       (expect toEqual [:on :unknown nil] @err))))
 ;?>
 
-(defn watch-ref [fsm trans r]
-  (swap! (:watchlist fsm) #(conj % r))
-  (add-watch r (:gensym fsm) #(act fsm trans :event %4)))
+(defn restart!
+  ""
+  [fsmr & args]
+  (swap! fsmr #(apply restart! % args)))
 
-(defn unwatch-ref [fsm r]
-  (swap! (:watchlist fsm) #(disj % r))
-  (remove-watch r (:gensym fsm)))
+(defn act!
+  ""
+  [fsmr trans & args]
+  (swap! fsmr #(apply act % trans args))
+  nil)
 
-(defn unwatch-all [fsm]
-  (let [g (:gensym fsm)]
-    (swap! (:watchlist fsm) #(doseq [r %] (remove-watch r g)))))
+(defn watch-ref [fsmr trans r]
+  (swap! fsmr (fn [fsm]
+                   (add-watch r (:gensym fsm)
+                              #(act! fsmr trans :event %4))
+                   (update-in fsm [:watchlist] #(conj % r))))
+  nil)
+
+(defn unwatch-ref [fsmr r]
+  (swap! fsmr (fn [fsm]
+                (remove-watch r (:gensym fsm))
+                (update-in fsm [:watchlist] #(disj % r))))
+  nil)
+
+(defn unwatch-all [fsmr]
+  (swap! fsmr (fn [fsm]
+                (update-in fsm [:watchlist]
+                           #(doseq [r %] (remove-watch r (:gensym fsm))))))
+  nil)
 
 ;<?
 (check "watch unwatch"
@@ -175,26 +188,27 @@
               {:on {:down :off}
                :off {:up :on}}
               :callback #(:event %))
+        fsmr (atom fsm)
         a (atom :x)]
     (it "should store watch"
-      (watch-ref fsm :down a)
-      (expect toBeTruthy (@(:watchlist fsm) a)))
+      (watch-ref fsmr :down a)
+      (expect toBeTruthy ((:watchlist @fsmr) a)))
     (it "should transition on watch change"
       (reset! a :y)
-      (expect toEqual :off (state fsm))
-      (expect toEqual :y @(:data fsm)))
+      (expect toEqual :off (:state @fsmr))
+      (expect toEqual :y (:data @fsmr)))
     (it "should unwatch"
-      (watch-ref fsm :up (atom :temp))
-      (unwatch-ref fsm a)
-      (expect toBeFalsy (@(:watchlist fsm) a))
-      (expect toEqual 1 (count @(:watchlist fsm))))
+      (watch-ref fsmr :up (atom :temp))
+      (unwatch-ref fsmr a)
+      (expect toBeFalsy ((:watchlist @fsmr) a))
+      (expect toEqual 1 (count (:watchlist @fsmr))))
     (it "should unwatch all"
-      (unwatch-all fsm)
-      (expect toEqual 0 (count @(:watchlist fsm))))
+      (unwatch-all fsmr)
+      (expect toEqual 0 (count (:watchlist @fsmr))))
     (it "should not transition after unwatching"
       (reset! a :z)
-      (expect toEqual :off (state fsm))
-      (expect-not toEqual :z @(:data fsm)))))
+      (expect toEqual :off (:state @fsmr))
+      (expect-not toEqual :z (:data @fsmr)))))
 ;?>
 
 #_(defn add-state
