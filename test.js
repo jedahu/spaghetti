442d447bdc91fdec2f99e1b2eacffb0ee63e3fc4
Jeremy Hughes <jedahu@gmail.com>
2011-12-12 22:39:09 +1300

    updated test.js

--- test.js
+++ test.js
@@ -2,9 +2,11 @@ phantom.injectJs('jasmine/jasmine.js');
 phantom.injectJs('jasmine/ConsoleReporter.js');
 phantom.injectJs('out/all.js');
 
+var fs = require('fs');
+
 jasmine.getEnv().addReporter(
     new jasmine.ConsoleReporter(
-      function(msg) { console.log(msg); },
+      function(msg) { fs.write('/dev/stdout', msg, 'w'); },
       function(runner) {
         if (runner.results().passed()) phantom.exit(0);
         else phantom.exit(1);
